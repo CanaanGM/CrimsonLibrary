fa4346e77b300727bba6afb93af762b62aeba659
Canaan <Reakak1@gmail.com>
2022-03-10 15:22:14 +0200

    Add: CORS

--- CrimsonLibrary/Startup.cs
+++ CrimsonLibrary/Startup.cs
@@ -1,17 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CrimsonLibrary
 {
@@ -27,8 +19,17 @@ namespace CrimsonLibrary
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddCors(
+                x =>
+                {
+                    x.AddPolicy("AllowAll", builder =>
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader()
+                        );
+                }
+            );
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CrimsonLibrary", Version = "v1" });
@@ -43,13 +44,14 @@ namespace CrimsonLibrary
                 app.UseDeveloperExceptionPage();
             }
 
-            // general docs 
+            // general docs
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CrimsonLibrary v1"));
 
-
             app.UseHttpsRedirection();
 
+            app.UseCors("AllowAll");
+            
             app.UseRouting();
 
             app.UseAuthorization();
